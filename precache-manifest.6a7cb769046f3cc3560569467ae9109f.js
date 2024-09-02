2f5c7505227dc235953fa4fa3f30519d7e85f781
MohanDev0024 <mohandev0024@gmail.com>
2024-09-03 01:58:58 +0530

    Updates

---
+++ precache-manifest.6a7cb769046f3cc3560569467ae9109f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a78f804bf7c1e5535a13c7620a3efe2",
+    "url": "/new-repo/index.html"
+  },
+  {
+    "revision": "67466023061ff3f0011e",
+    "url": "/new-repo/static/css/main.02f784cd.chunk.css"
+  },
+  {
+    "revision": "4309d6dc123f3d8f746d",
+    "url": "/new-repo/static/js/2.6c811e04.chunk.js"
+  },
+  {
+    "revision": "67466023061ff3f0011e",
+    "url": "/new-repo/static/js/main.2afc038a.chunk.js"
+  },
+  {
+    "revision": "27e56b6e8f3252a9737c",
+    "url": "/new-repo/static/js/runtime~main.018c1a49.js"
+  }
+]);
